20e8b2da062083bf3114f1f8134c5a0b11fb3896
Vedanand <vvinny021@gmail.com>
2026-04-19 23:17:52 +0530

    update login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -41,13 +41,8 @@ const login = () => {
         onChange={e=>setEmail(e.target.value)}
         required />
       </div>
-
-      <div className="relative mb-4">
-        <label html="password" className="leading-7 text-sm text-gray-600">Password</label>
-        <input type="password" id="password" name="password" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" value={password}
-        onChange={e=>setPassword(e.target.value)}
-        required />
-      </div>
+      
+      
       <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" disabled={btnLoading}>
         {btnLoading ? "Submitting..." : "Button"}
       </button>
