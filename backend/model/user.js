39f6bbc122e77fdea07614377576632d23578277
Vedanand <vvinny021@gmail.com>
2026-05-10 23:55:24 +0530

    change user model 2

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,24 +1,7 @@
 import mongoose from "mongoose";
 
 const schema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
-  },
+  
 });
 
 export const User = mongoose.model("User", schema);
