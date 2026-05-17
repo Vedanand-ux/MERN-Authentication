bd436c44588a27c315fa8f017d67f8a747bada35
Vedanand <vvinny021@gmail.com>
2026-05-17 23:55:49 +0530

    new user schema

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,7 +7,10 @@ export const Home = () => {
   const {logoutUser} = AppData();
   const navigate = useNavigate();
   return (
-
+    <div className = "flex w-100px m-auto mt-40">
+      <div>home</div>
+      <button className="bg-red-500 text-white p-2 rounded-md" onClick={() => logoutUser(navigate)}>logout</button>
+    </div>
   )
 }
 
